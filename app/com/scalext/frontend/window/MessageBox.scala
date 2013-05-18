7d38bb6d79b22280992031165e2f5c9a2c70bb71
Roy van Kaathoven <royvankaathoven@hotmail.com>
2013-05-18 19:14:11 +0200

    Simple messagebox test

---
+++ app/com/scalext/frontend/window/MessageBox.scala
@@ -0,0 +1,27 @@
+package com.scalext.frontend.window
+
+import com.scalext.frontend.Base
+import play.api.libs.json.Json
+
+object MessageBox {
+  def confirm(message: String, title: String = "Confirm"): MessageBox = {
+    new MessageBox {
+      title = title
+      message = message
+    }
+  }
+}
+
+case class MessageBox() extends Base {
+
+  var title = ""
+  var message = ""
+
+  override def toJson = Json.obj(
+    "title" -> title,
+    "message" -> message)
+
+  def show = s"Ext.MessageBox.show(${toJson});"
+
+  override def toString = show
+}
