1b857e0894c6f0e85b8d4a32b7dbcc1f16b28571
Roy van Kaathoven <royvankaathoven@hotmail.com>
2013-05-23 23:33:37 +0200

    moved assets to example project

--- public/desktop/js/ShortcutModel.js
+++
@@ -1,8 +0,0 @@
-Ext.define('Ext.ux.desktop.ShortcutModel', {
-    extend: 'Ext.data.Model',
-    fields: [
-       { name: 'name' },
-       { name: 'iconCls' },
-       { name: 'module' }
-    ]
-});
