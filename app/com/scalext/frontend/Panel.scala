2fe978f6079e164893f41010f0c5ca9ebd643115
Roy van Kaathoven <royvankaathoven@hotmail.com>
2013-06-09 11:43:32 +0200

    testing component generator

--- app/com/scalext/frontend/Panel.scala
+++ app/com/scalext/frontend/Panel.scala
@@ -2,13 +2,17 @@ package com.scalext.frontend
 
 
 case class Panel() extends Container {
-
   var title = ""
+  val className = "Ext.Panel"
+
+  var width = 0
+  var height = 0
 
   override def toJson = {
     config += "title" -> title
-    if (!items.isEmpty)
-      config += ("items" -> items)
+    if (width > 0) config += "width" -> width
+    if (height > 0) config += "height" -> height
+    if (!items.isEmpty) config += ("items" -> items)
     super.toJson
   }
 
