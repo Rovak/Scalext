83b62b0172044299157f127ac30738acefd1a942
Roy van Kaathoven <royvankaathoven@hotmail.com>
2014-02-23 12:59:08 +0100

    replaced play project with sbt project file

--- project/Build.scala
+++ project/Build.scala
@@ -1,23 +1,26 @@
 import sbt._
 import Keys._
-import play.Project._
 
 object ApplicationBuild extends Build {
 
-  val appName         = "scalaext"
-  val appVersion      = "0.1.1"
+  val appName         = "scalext"
+  val projectVersion = "0.1-SNAPSHOT"
 
-  val appDependencies = Seq(
-    jdbc,
-    anorm,
-    "commons-io" % "commons-io" % "2.1",
-    "org.apache.commons" % "commons-lang3" % "3.1",
-    "com.google.code.gson" % "gson" % "2.2.3"
-  )
+  val defaultSettings = Project.defaultSettings ++ Seq(
+    resolvers += "Typesafe Repository" at "http://repo.typesafe.com/typesafe/releases/",
+    version :=  "0.1-SNAPSHOT",
+    scalaVersion := "2.10.2")
 
-
-  val main = play.Project(appName, appVersion, appDependencies).settings(
-
-  )
+  val main = Project(
+    id = appName,
+    base = file("scalext"),
+    settings = defaultSettings ++ Seq(
+      libraryDependencies ++= Seq(
+        "commons-io" % "commons-io" % "2.1",
+        "org.apache.commons" % "commons-lang3" % "3.1",
+        "com.google.code.gson" % "gson" % "2.2.3",
+        "com.typesafe.play" %% "play-json" % "2.2.1"
+      )
+    ))
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,8 +1,3 @@
-// Comment to get more information during initialization
-logLevel := Level.Warn
+resolvers += "Sonatype snapshots" at "http://oss.sonatype.org/content/repositories/snapshots/"
 
-// The Typesafe repository 
-resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
-
-// Use the Play sbt plugin for Play projects
-addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.2.0")
+addSbtPlugin(dependency="com.github.mpeltonen" % "sbt-idea" % "1.6.0")
